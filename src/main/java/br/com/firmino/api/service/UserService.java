afa3115898d183430d2504de86f78c4654000206
Silbert Firmino <silbert.fcds@hotmai.com>
2022-11-06 08:00:58 -0300

    Criando metodo create

--- src/main/java/br/com/firmino/api/service/UserService.java
+++ src/main/java/br/com/firmino/api/service/UserService.java
@@ -3,9 +3,11 @@ package br.com.firmino.api.service;
 import java.util.List;
 
 import br.com.firmino.api.domain.User;
+import br.com.firmino.api.domain.dto.UserDTO;
 
 public interface UserService {
 
 	User findById(Integer id);
 	List<User> findAll();
+	User create(UserDTO obj);
 }
--- src/main/java/br/com/firmino/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/firmino/api/service/impl/UserServiceImpl.java
@@ -3,10 +3,12 @@ package br.com.firmino.api.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.firmino.api.domain.User;
+import br.com.firmino.api.domain.dto.UserDTO;
 import br.com.firmino.api.exception.ObjectNotFoundException;
 import br.com.firmino.api.repository.UserRepository;
 import br.com.firmino.api.service.UserService;
@@ -17,6 +19,9 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository repository;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@Override
 	public User findById(Integer id) {
 		Optional<User> userOptional = repository.findById(id);
@@ -25,6 +30,11 @@ public class UserServiceImpl implements UserService{
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+
+	@Override
+	public User create(UserDTO obj) {
+		return repository.save(mapper.map(obj, User.class));
 	}
 
 }
